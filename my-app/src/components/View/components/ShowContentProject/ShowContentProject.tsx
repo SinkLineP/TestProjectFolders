17d129a3e9c39680e6cb4b9858840339953288eb
SinkLine_P <popovartem505@gmail.com>
2022-11-17 23:09:18 +0300

    fix show all Structures

--- my-app/src/components/View/components/ShowContentProject/ShowContentProject.tsx
+++ my-app/src/components/View/components/ShowContentProject/ShowContentProject.tsx
@@ -23,16 +23,47 @@ const ShowContentProject: React.FC<ShowContentType> = ({object}) => {
         {
           objectArray.map((objectItem: any, index: number) => {
             if (objectItem.rowName !== "error") {
-              return (
-                <tr key={index} className={"project-input"}>
-                  <td>level</td>
-                  <td>{objectItem.rowName === null ? (<input className={"input-edit"} placeholder={"Введите наименование.."} type={"string"} />) : objectItem.rowName}</td>
-                  <td>{objectItem.salary === null ? (<input className={"input-edit"} placeholder={"Введите основую з/п.."} type={"string"} />) : objectItem.salary}</td>
-                  <td>{objectItem.equipmentCosts === null ? (<input className={"input-edit"} placeholder={"Введите кол-во оборудования.."} type={"string"} />) : objectItem.equipmentCosts}</td>
-                  <td>{objectItem.overheads === null ? (<input className={"input-edit"} placeholder={"Введите накладные расходы.."} type={"string"} />) : objectItem.overheads}</td>
-                  <td>{objectItem.estimatedProfit === null ? (<input className={"input-edit"} placeholder={"Введите сметную прибыль.."} type={"string"} />) : objectItem.estimatedProfit}</td>
-                </tr>
-              )
+              if (objectItem.child[0] !== null) {
+                return (
+                  <>
+                    <tr key={index} className={"project-input"}>
+                      <td>level</td>
+                      <td>{objectItem.rowName === null ? (<input className={"input-edit"} placeholder={"Введите наименование.."} type={"string"}/>) : objectItem.rowName}</td>
+                      <td>{objectItem.salary === null ? (<input className={"input-edit"} placeholder={"Введите основую з/п.."} type={"string"}/>) : objectItem.salary}</td>
+                      <td>{objectItem.equipmentCosts === null ? (<input className={"input-edit"} placeholder={"Введите кол-во оборудования.."} type={"string"}/>) : objectItem.equipmentCosts}</td>
+                      <td>{objectItem.overheads === null ? (<input className={"input-edit"} placeholder={"Введите накладные расходы.."} type={"string"}/>) : objectItem.overheads}</td>
+                      <td>{objectItem.estimatedProfit === null ? (<input className={"input-edit"} placeholder={"Введите сметную прибыль.."} type={"string"}/>) : objectItem.estimatedProfit}</td>
+                    </tr>
+                    {
+                      objectItem.child.map((childItem: any, indexChild: number) => {
+                        return (
+                          <tr key={indexChild} className={"project-input"}>
+                            <td>level 2</td>
+                            <td>{childItem.rowName === null ? (<input className={"input-edit"} placeholder={"Введите наименование.."} type={"string"}/>) : childItem.rowName}</td>
+                            <td>{childItem.salary === null ? (<input className={"input-edit"} placeholder={"Введите основую з/п.."} type={"string"}/>) : childItem.salary}</td>
+                            <td>{childItem.equipmentCosts === null ? (<input className={"input-edit"} placeholder={"Введите кол-во оборудования.."} type={"string"}/>) : childItem.equipmentCosts}</td>
+                            <td>{childItem.overheads === null ? (<input className={"input-edit"} placeholder={"Введите накладные расходы.."} type={"string"}/>) : childItem.overheads}</td>
+                            <td>{childItem.estimatedProfit === null ? (<input className={"input-edit"} placeholder={"Введите сметную прибыль.."} type={"string"}/>) : childItem.estimatedProfit}</td>
+                          </tr>
+                        )
+                      })
+                    }
+                  </>
+                )
+              } else {
+                return (
+                  <>
+                    <tr key={index + 1} className={"project-input"}>
+                      <td>level</td>
+                      <td>{objectItem.rowName === null ? (<input className={"input-edit"} placeholder={"Введите наименование.."} type={"string"}/>) : objectItem.rowName}</td>
+                      <td>{objectItem.salary === null ? (<input className={"input-edit"} placeholder={"Введите основую з/п.."} type={"string"}/>) : objectItem.salary}</td>
+                      <td>{objectItem.equipmentCosts === null ? (<input className={"input-edit"} placeholder={"Введите кол-во оборудования.."} type={"string"}/>) : objectItem.equipmentCosts}</td>
+                      <td>{objectItem.overheads === null ? (<input className={"input-edit"} placeholder={"Введите накладные расходы.."} type={"string"}/>) : objectItem.overheads}</td>
+                      <td>{objectItem.estimatedProfit === null ? (<input className={"input-edit"} placeholder={"Введите сметную прибыль.."} type={"string"}/>) : objectItem.estimatedProfit}</td>
+                    </tr>
+                  </>
+                )
+              }
             }
           })
         }
