5ff84176189136f142e28381ec5f33b398fc790d
SinkLine_P <popovartem505@gmail.com>
2022-11-17 15:49:35 +0300

    created showContentProject

--- my-app/src/components/View/View.tsx
+++ my-app/src/components/View/View.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import ButtonNavbar from "../Buttons/ButtonNavbar/ButtonNavbar";
 import "./styles/index.scss";
 import arrowDown from "./icons/arrowDown.svg";
+import ShowContentProject from "./components/ShowContentProject/ShowContentProject";
 
 
 function View() {
@@ -9,13 +10,22 @@ function View() {
     id: "",
     class: "",
   });
+  const [arrayEntity, setArrayEntity] = useState([]);
+  const arrayEntityTemplate = [{
+    level: 0,
+    rowName: "",
+    salary: 0,
+    equipmentCosts: 0,
+    overheads: 0,
+    estimatedProfit: 0
+  }]
 
   return (
     <div className={"show-content"}>
       <table className={"table"}>
         <tbody>
           <tr className={"table-navbar"}>
-            <td className={"table-navbar-buttons"} colSpan={2}>
+            <td className={"td table-navbar-buttons"} colSpan={2}>
               <ButtonNavbar idBtn={"menu"} content={"Menu"} setCurrentBtn={setCurrentBtn} activeClass={currentBtn.id === "menu" ? currentBtn.class + " btn-active" : ""}/>
               <ButtonNavbar idBtn={"back"} content={"Back"} setCurrentBtn={setCurrentBtn} activeClass={currentBtn.id === "back" ? currentBtn.class + " btn-active" : ""}/>
               <ButtonNavbar idBtn={"show"} content={"Просмотр"} setCurrentBtn={setCurrentBtn} activeClass={currentBtn.id === "show" ? currentBtn.class + " btn-active" : ""}/>
@@ -23,7 +33,7 @@ function View() {
             </td>
           </tr>
           <tr className={"table-titles"}>
-            <td className={"table-name-or-sorting"}>
+            <td className={"td table-name-or-sorting"}>
               <div className="flex-container">
                 <div className="item1">
                   <p className={"name-project"}>Название проекта</p>
@@ -36,11 +46,13 @@ function View() {
                 </div>
               </div>
             </td>
-            <td className={"table-name-project"}></td>
+            <td className={"td table-name-project"}></td>
           </tr>
           <tr className={"table-content"}>
-            <td className={"table-list-projects"}></td>
-            <td className={"table-structure-folder"}></td>
+            <td className={"td table-list-projects"}></td>
+            <td className={"td table-structure-folder"}>
+              <ShowContentProject array={arrayEntityTemplate} />
+            </td>
           </tr>
         </tbody>
       </table>
---
+++ my-app/src/components/View/components/ShowContentProject/ShowContentProject.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Styles/index.scss";
+
+interface ShowContentType {
+  array: any;
+}
+
+const ShowContentProject: React.FC<ShowContentType> = ({array}) => {
+
+  return (
+    <table className={"table-show-content"}>
+      <tbody>
+        <tr className={"table-projects"}>
+          <td className={"level"}>Уровень</td>
+          <td className={"rowName"}>Наименование работ</td>
+          <td className={"salary"}>Основная з/п</td>
+          <td className={"equipmentCosts"}>Оборудование</td>
+          <td className={"overheads"}>Накладные расходы</td>
+          <td className={"estimatedProfit"}>Сметная прибыль</td>
+        </tr>
+        {
+          array.map((item: any) => {
+            return (
+              <tr className={"project-input"}>
+                <td>{item.level}</td>
+                <td>{item.rowName}</td>
+                <td>{item.salary}</td>
+                <td>{item.equipmentCosts}</td>
+                <td>{item.overheads}</td>
+                <td>{item.estimatedProfit}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
+
+}
+
+export default ShowContentProject;
